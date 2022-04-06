4d3f56d81323ec6b4c033b454df34f8802f79814
rfkim17 <rifkimaulana0010@gmail.com>
2022-04-06 13:18:26 +0700

    tampilan

---
+++ views/beranda.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800" align="center">SELAMAT DATANG DI PERPUSTAKAAN PERMATA</h1>
+    
+
+</div>           
